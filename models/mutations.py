fe833e8a5ed6a6660872e0e07b7ecf376cc6f32d
dana <dana@225pc140.cvr.gla.ac.uk>
2025-06-02 14:38:03 +0100

    Big fix: mutations using correct sequence features

--- models/mutations.py
+++ models/mutations.py
@@ -46,9 +46,8 @@ class Mutations:
         self.region = region
 
         alignments = self.__get_alignments()
-        master_reference = self.__get_master_reference()
 
-        mutations = self.__parse_mutations(alignments, master_reference)
+        mutations = self.__parse_mutations(alignments)
         return mutations
 
     def get_mutation_regions_and_codons(self):
@@ -60,12 +59,13 @@ class Mutations:
         """
         master_reference = self.__get_master_reference()
         for feature in master_reference:
-            codon_start, codon_end = get_codon_labeling(feature["ref_start"], feature["ref_end"])
+            codon_start, codon_end = get_codon_labeling(feature["cds_start"], feature["cds_end"])
             feature["codon_start"] = codon_start
             feature["codon_end"] = codon_end
+            feature["proudct"] = feature["product"]
         return master_reference
 
-    def __parse_mutations(self, alignments, master_reference):
+    def __parse_mutations(self, alignments):
         """
         Annotate mutations by comparing aligned codons to the reference.
 
@@ -76,22 +76,26 @@ class Mutations:
         Returns:
             list: Annotated alignment records with mutations.
         """
-        ref_start = master_reference[0]["cds_start"]
-        ref_end = master_reference[0]["cds_end"]
 
         for alignment in alignments:
+            ref_start = alignment["cds_start"]
+            ref_end = alignment["cds_end"]
             alignment["mutations"] = {}
             sub_seq = alignment["alignment"][ref_start:ref_end+1]
             codons_new = [sub_seq[i:i+3] for i in range(0, len(sub_seq), 3)]
 
             for codon in self.codons:
-                selected_codon = codons_new[int(codon)]
-                if "-" not in selected_codon:
-                    if len(selected_codon) % 3 == 0:
-                        protein = translateCodon(selected_codon)
-                        alignment["mutations"][str(codon)] = protein
-                elif selected_codon != "---":
-                    alignment["mutations"][str(codon)] = "X"
+                if 0 <= int(codon) < len(codons_new):
+                    selected_codon = codons_new[int(codon)]
+                    if "-" not in selected_codon:
+                        if len(selected_codon) % 3 == 0:
+                            protein = translateCodon(selected_codon)
+                            alignment["mutations"][str(codon)] = protein
+                    elif selected_codon != "---":
+                        alignment["mutations"][str(codon)] = "X"
+
+                else:
+                    alignment["mutations"][str(codon)] = ""
 
         return alignments
 
@@ -104,16 +108,18 @@ class Mutations:
         """
         formatted_hosts = ', '.join(['%s'] * len(self.hosts))
         query = f'''
-            SELECT s.*, m.* 
+            SELECT s.*, m.*, f.* 
             FROM sequence_alignment s 
             LEFT JOIN meta_data m ON s.sequence_id = m.primary_accession 
-            WHERE m.host IN ({formatted_hosts});
+            LEFT JOIN features f ON s.sequence_id = f.accession 
+            WHERE m.host IN ({formatted_hosts}) 
+            AND f.product = '{self.region}';
         '''
 
         with connections[self.database].cursor() as cursor:
             cursor.execute(query, self.hosts)
             alignments = dictfetchall(cursor)
-
+        print(alignments)
         return alignments
 
     def __get_master_reference(self):

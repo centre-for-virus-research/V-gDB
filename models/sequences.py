c2b08357bdd7f5fcd68e257f06b34d8357d4e822
dana <dana@225pc140.cvr.gla.ac.uk>
2026-03-19 15:05:44 +0000

    fixed references list filter bug

--- models/sequences.py
+++ models/sequences.py
@@ -379,7 +379,7 @@ class Sequences:
             }
         
         taxonomy_filters = ['phylum', 'class', 'order_category', 'family', 'genus', 'species']
-        standard_filters = ['primary_accession', 'isolate', 'exclusion_status', 'EPA_major_clade', 'EPA_minor_clade']
+        standard_filters = ['primary_accession', 'isolate', 'exclusion_status', 'EPA_major_clade', 'EPA_minor_clade', 'accession_type']
         region_filters = ['country']
 
         where_clauses, params = [], []
